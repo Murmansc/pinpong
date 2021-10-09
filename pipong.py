cd31a1922a67db11f5c52f97021e56ac5ae855db
Murmansc <90508584+Murmansc@users.noreply.github.com>
2021-10-09 14:23:44 +0300

    Update pipong.py

--- pipong.py
+++ pipong.py
@@ -1,7 +1,7 @@
 from pygame import * 
 from ay import Player
 from qq import GameSprite
-
+from time import sleep
 
 def game():
     back = (200, 255, 255)
@@ -15,16 +15,22 @@ def game():
     clock = time.Clock()
     FPS = 60
 
-    racket1 = Player('racket.png', 30, 200, 50, 150, 4, win_width, win_height, window)
-    racket2 = Player('racket.png', 520, 200, 50, 150, 4, win_width, win_height, window)
+    racket1 = Player('racket.png', 30, 200, 30, 100, 4, win_width, win_height, window)
+    racket2 = Player('racket.png', 520, 200, 30, 100, 4, win_width, win_height, window)
     ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 4, win_width, win_height, window)
-    speed_x = 3
-    speed_y = 3
+    speed_x = 5
+    speed_y = 5
     font.init()
     font1 = font.Font(None, 35)
-    font1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
-    font2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+    lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+    lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+    window.fill(back)
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+    display.update()
 
+    sleep(1)
     while run:
         for e in event.get():
             if e.type == QUIT:
@@ -35,14 +41,22 @@ def game():
             racket2.reset()
             ball.reset()
             if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-                speed_x += -1
+                speed_x *= -1
             
             if ball.rect.y > win_height-50 or ball.rect.y < 0:
-                speed_y += -1
+                speed_y *= -1
             ball.rect.x += speed_x
             ball.rect.y += speed_y
             racket2.update_r()
             racket1.update_l()
+
+            if ball.rect.x < 0:
+                finish = True
+                window.blit(lose1, (200, 200))
+            
+            if ball.rect.x > win_width:
+                finish = True
+                window.blit(lose2, (200, 200))
         display.update()
         clock.tick(FPS)
 if __name__ == '__main__':
